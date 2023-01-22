a81dcb996e8428f93ad19f47b5b245b712cfaa45
Stoffel van Aswegen <stoffel@zeniton.co.za>
2023-01-22 11:11:21 +0200

    Remove junk motor stuff

--- include/motor.h
+++ include/motor.h
@@ -1,12 +1,6 @@
 #ifndef __MOTOR__
 #define __MOTOR__
 
-#define DRIVER TMC2209Stepper //Set the actual stepper driver in use
-
 void setupMotor();
-void setSpeed(int);
-void setAcceleration(int);
-void setAmps(int);
-void MotorTask(void *);
 
 #endif
