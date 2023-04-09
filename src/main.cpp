90986a4dcf54477bcff8f12da30bf13ee00b5d72
Stoffel van Aswegen <stoffel@zeniton.co.za>
2023-04-09 12:18:42 +0200

    Serve webpage from SPIFFS

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,15 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
-#include <WebServer.h>
-#include <WebSocketsServer.h>
 #include <HardwareSerial.h>
+#include "ESPAsyncWebServer.h"
+#include "SPIFFS.h"
 #include "sdcard.h"
 
 #include "main.h"
 #include "motor.h"
 
 // Devices
-static WebServer webserver(80);
-static WebSocketsServer websocket = WebSocketsServer(81);
+static AsyncWebServer webserver(80);
 static HardwareSerial scale(1);
 static hw_timer_t *timer = NULL;
 static Motor motor;
@@ -38,9 +37,8 @@ void toggleLED();
 void setupWiFi(bool useLocal);
 void updateData();
 void showSchedule();
-void setupWeb(WebServer &, WebSocketsServer &, WebSocketsServerCore::WebSocketServerEvent);
-void sendData(WebSocketsServer &, const Data_t *);
-void onWebSocketEvent(byte, WStype_t, uint8_t *, size_t);
+void setupWeb();
+// void sendData(WebSocketsServer &, const Data_t *);
 void setupScale(HardwareSerial &);
 char *getWeight(HardwareSerial &scale, char (&)[64]);
 void setupSDCard();
@@ -55,8 +53,16 @@ void IRAM_ATTR onAlarm()
 void setup()
 {
     Serial.begin(115200);
+
+    if (!SPIFFS.begin(true))
+    {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+    }
+    Serial.println("SPIFFS mounted successfully");
+
     setupWiFi(true);
-    setupWeb(webserver, websocket, onWebSocketEvent);
+    setupWeb();
     setupLED();
     setupScale(scale);
     motor.diagnose();
@@ -78,15 +84,12 @@ void loop()
     const int moment = 500; // ms
     static unsigned long earlier = 0;
 
-    webserver.handleClient();
-    websocket.loop();
-
     updateData();
 
     auto w = getWeight(scale, weight);
     sdcard.log(data.rpm, w);
 
-    sendData(websocket, &data);
+    // sendData(websocket, &data);
 
     if (done)
         return;
@@ -115,38 +118,6 @@ void showSchedule()
     update = false;
 }
 
-void onWebSocketEvent(byte num, WStype_t type, uint8_t *payload, size_t length)
-{
-    switch (type)
-    {
-    case WStype_DISCONNECTED:
-        Serial.println("Client disconnected");
-        break;
-
-    case WStype_CONNECTED:
-        Serial.println("Client connected");
-        sendData(websocket, &data);
-        break;
-
-    case WStype_TEXT:
-        Serial.printf("Request received: %s\n", payload);
-
-        StaticJsonDocument<32> req;
-        deserializeJson(req, payload);
-
-        auto id = req["id"];
-        if (id == "restart")
-            ESP.restart();
-        else if (id == "led")
-            toggleLED();
-        else if (id == "rpm")
-            motor.setRPM(atof(req["value"]));
-        else if (id == "usteps")
-            motor.setMicroSteps((MICROSTEPS)atoi(req["value"]));
-        break;
-    }
-}
-
 void updateData()
 {
     data.led = getLED() == HIGH;
@@ -154,4 +125,15 @@ void updateData()
     data.microSteps = motor.getMicroSteps();
     data.rpm = motor.getRPM();
     data.speed = motor.getSpeed();
+}
+
+void setupWeb()
+{
+    webserver.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+                 { request->send(SPIFFS, "/index.html", "text/html"); });
+
+    webserver.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request)
+                 { request->send(SPIFFS, "/style.css", "text/css"); });
+
+    webserver.begin(); 
 }
